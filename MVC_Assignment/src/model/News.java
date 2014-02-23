e7ef153547c1bb0a8ce78bb4ce33ef5147d2f20c
bbaburaj <bbaburaj@asu.edu>
2014-02-23 06:26:25 -0700

    build files, web.xml files and resource folder creater. Login.html page created

--- MVC_Assignment/src/model/News.java
+++ MVC_Assignment/src/model/News.java
@@ -1,5 +1,44 @@
 package model;
 
 public class News {
+	private String author;
+	private String type;
+	private String title;
+	private String story;
+	
+	public News() {
+		
+	}
+	public News(String author, String type, String title, String story) {
+		super();
+		this.author = author;
+		this.type = type;
+		this.title = title;
+		this.story = story;
+	}
 
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStory() {
+		return story;
+	}
+	public void setStory(String story) {
+		this.story = story;
+	}
 }
